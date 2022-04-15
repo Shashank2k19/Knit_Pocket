e277c45eabad191b057c302ef06fe9329abf285b
shashank shekhar <mr.shekhar008@gmail.com>
2022-04-15 13:54:10 +0530

    final commit

--- mail.php
+++ mail.php
@@ -11,7 +11,7 @@ $reffer=    $_POST['reffer'];
 $intrest=   $_POST['intrest'];
 
 // Email text
-$to = "mr.shekhar008@gmail.com";
+$to = "hello@knitpocket.com";
 $subject = "New From Submission on Knit Pocket ";
 
 $txt =" Name : ". $name . "\r\n Email : " . $email . "\r\n Contact No : " . $phone ."\r\n Whatsapp No : " . $whatsapp . "\r\n Can you describe your skill sets and the kind of freelance projects you would like to be a part of ? = " . $skill .  "\r\n Can you please share a drive link/ website to showcase your past work or portfolio = " . $drivelink .  "\r\n Referral Incentive : " . $reffer .  "\r\n Would you be interested in learning more ? : " . $intrest;
